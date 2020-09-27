8bc663eed6beabba23a90f7e3d9a490f6bb80147
Jonathan <jonathansapp08@gmail.com>
2020-09-27 19:17:12 -0400

    adding missing logging

--- home-flask/main.py
+++ home-flask/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, url_for, jsonify, render_template, redirect
+import logging
 import os
 
 from todo import Todo
@@ -17,31 +18,47 @@ def index():
     lights = light.get()
 
     if request.method == 'POST' and 'addtask' in request.form:
-        task = request.form['addtask']
-        todo.post(task)
-        tasks = todo.get()
-        return render_template("index.html", tasks=tasks, lights=lights)
+        try:
+            task = request.form['addtask']
+            todo.post(task)
+            tasks = todo.get()
+            app.logger.info("Home: Added todo: " + str(task))
+            return render_template("index.html", tasks=tasks, lights=lights)
+        except:
+            app.logger.info("Home: Failed to add todo: " + str(task))
 
     if request.method == 'POST' and 'deletetask' in request.form:
-        task = request.form['deletetask']
-        todo.delete(task)
-        tasks = todo.get()
-        return render_template("index.html", tasks=tasks, lights=lights)
+        try:
+            task = request.form['deletetask']
+            todo.delete(task)
+            tasks = todo.get()
+            app.logger.info("Home: Deleted todo: " + str(task))
+            return render_template("index.html", tasks=tasks, lights=lights)
+        except:
+            app.logger.info("Home: Failed to delete todo: " + str(task))
 
     if request.method == 'POST' and 'remote' in request.form:
-        action = request.form['remote']
-        tv.control(action=action)
-        return render_template("index.html", tasks=tasks, lights=lights)   
+        try:
+            action = request.form['remote']
+            tv.control(action=action)
+            app.logger.info("Home: Remote action success: " + str(action))
+            return render_template("index.html", tasks=tasks, lights=lights)   
+        except:
+            app.logger.info("Home: Remote action failed: " + str(action))
 
     if request.method == 'POST' and 'toggle' in request.form:
-        light_id = request.form['toggle']
-        current_state = lights[light_id]['state']['on']
-        light.toggle(light_id=light_id, current_state=current_state)
-        lights = light.get()
-        return render_template("index.html", tasks=tasks, lights=lights)
+        try:
+            light_id = request.form['toggle']
+            current_state = lights[light_id]['state']['on']
+            light.toggle(light_id=light_id, current_state=current_state)
+            lights = light.get()
+            app.logger.info("Home: Success in toggling light: " + str(light_id))
+            return render_template("index.html", tasks=tasks, lights=lights)
+        except:
+            app.logger.info("Home: Failed to toggle light: " + str(light_id))
 
     return render_template("index.html", tasks=tasks, lights=lights)
 
 
 if __name__ == "__main__": 
-    app.run(host ='0.0.0.0', port=5001, debug = True) 
+    app.run(host ='0.0.0.0', port=5001, debug=True) 
